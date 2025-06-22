245953b824cc897e42309d216d84f1395c8a92e1
Debora Kellenberger <debkellen1@gmail.com>
2025-06-22 17:38:51 -0300

    commit

--- 05_Join/02_left_join.sql
+++ 05_Join/02_left_join.sql
@@ -11,7 +11,17 @@ SELECT name, dni_number FROM users
 LEFT JOIN dni
 ON users.user_id = dni.user_id;
 
---Selecciona todos los dni junto co el nombre de su respectivo usuario (lo tenga o no)
+--Selecciona todos los dni junto con el nombre de su respectivo usuario (lo tenga o no)
 SELECT name, dni_number FROM dni
 LEFT JOIN users 
 ON users.user_id = dni.user_id;
+
+-- Selecciona todos los usuarios junto con sus lenguajes que maneja (los tenga o no)
+SELECT users.name, languages.name FROM users
+LEFT JOIN users_languages ON users.user_id = users_languages.user_id
+LEFT JOIN languages ON users_languages.language_id = languages.language_id;
+
+-- Selecciona todos los lenguajes junto con el usuario que lo maneja (lo tenga o no)
+SELECT languages.name, users.name FROM languages
+LEFT JOIN users_languages ON languages.language_id = users_languages.language_id
+LEFT JOIN users ON users_languages.user_id = users.user_id;
