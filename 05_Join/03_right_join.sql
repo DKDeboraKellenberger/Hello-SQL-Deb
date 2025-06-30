4a16d798cf16600811c5a48232e35666312f55fb
Debora Kellenberger <debkellen1@gmail.com>
2025-06-30 17:23:47 -0300

    commit

--- 05_Join/03_right_join.sql
+++ 05_Join/03_right_join.sql
@@ -12,8 +12,12 @@ SELECT name, dni_number FROM users
 RIGHT JOIN dni 
 ON users.user_id = dni.user_id;
 
--- Obtiene todos los nombres de los usuarios con sus nueros de dni, siempre que el dni tenga un user_id
+-- Obtiene todos los nombres de los usuarios con sus numeros de dni, siempre que el dni tenga un user_id
 SELECT name, dni_number FROM dni
 RIGHT JOIN users
 ON users.user_id = dni.user_id;
 
+-- Obtiene todos los lenguajes con los usuarios que lo usan, tengan usuario o no (SI NO TIENEN USUARIO SE COMPLETA CON NULL)
+SELECT users.name, languages.name FROM users
+RIGHT JOIN users_languages ON users.user_id = users_languages.user_id
+RIGHT JOIN languages ON users_languages.language_id = languages.language_id;
